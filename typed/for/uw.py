c0e1928b1644f4a8fab10b10221f31d2bbf0689b
N. J <70967044+coobeliues@users.noreply.github.com>
2021-06-25 16:17:38 +0600

    sheesh

--- typed/for/uw.py
+++ typed/for/uw.py
@@ -1,14 +1,14 @@
-prev = -1
-curr_rep_len = 0
-max_rep_len = 0
-element = int(input())
-while element != 0:
-    if prev == element:
-        curr_rep_len += 1
+prev=-123
+maxorigin=0
+maxi=0
+x=int(input())
+while x!=0:
+    if prev==x:
+        maxi+=1
     else:
-        prev = element
-        max_rep_len = max(max_rep_len, curr_rep_len)
-        curr_rep_len = 1
-    element = int(input())
-max_rep_len = max(max_rep_len, curr_rep_len)
-print(max_rep_len)
+        prev=x
+        maxorigin=max(maxi, maxorigin)
+        maxi=1
+    x=int(input())
+maxorigin=max(maxi, maxorigin)
+print(maxorigin)
