b4c9d8b0363df38d807e72b950acb1ad4b40b9b5
N. J <70967044+Coobeliues@users.noreply.github.com>
2021-06-25 16:49:21 +0600

    sheesh

--- typed/for/vw.py
+++ typed/for/vw.py
@@ -5,7 +5,6 @@ n1 = int(input())
 while n1:    
     m = max(inc, dec, m)
     n2 = int(input())
-    if not n2: break
     if n1 > n2: dec, inc = dec + 1, 1               
     elif n1 < n2: inc, dec = inc + 1, 1        
     else: inc = dec = 1
