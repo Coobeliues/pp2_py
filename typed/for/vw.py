dc58aa4e3480f9e994ac062230ff71daabe227fa
N. J <70967044+Coobeliues@users.noreply.github.com>
2021-06-25 16:55:10 +0600

    sheesh

--- typed/for/vw.py
+++ typed/for/vw.py
@@ -1,13 +1,12 @@
-m = 1
-dec = 1
-inc = 1
-n1 = int(input())
-while n1:    
-    m = max(inc, dec, m)
-    n2 = int(input())
-    if n1 > n2: dec, inc = dec + 1, 1               
-    elif n1 < n2: inc, dec = inc + 1, 1        
-    else: inc = dec = 1
-    n1 = n2
-print(m)
-#hi debil
+maxi= dec= inc =1
+prev=int(input())
+while prev:
+    maxi=max(inc, dec, maxi)
+    x=int(input())
+    if prev>x:
+        dec, inc= dec+1, 1
+    elif prev<x:
+        inc, dec=inc+1, 1
+    else: inc=dec=1
+    prev=x
+print(maxi)
